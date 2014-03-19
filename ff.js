26ae5cf47b47d82931786f95e05811ccbf64824c
Evan Simmons <esims89@gmail.com>
2014-03-19 10:50:34 -0700

    Factored out FF.

---
+++ ff.js
@@ -0,0 +1,122 @@
+
+function fordFulkerson(vs, es) {
+  es = es.slice(0).map(function(o) { return JSON.parse(JSON.stringify(o)); });
+  var rG = initResidual(vs,es),
+      path;
+
+  // Initialize a residual graph structure.
+  // returns a map of vetex id's to an array of their outgoing edges.
+  function initResidual(vs,es) {
+    var g = d3.map(),
+        outgoing,
+        incoming;
+
+    vs.forEach(function(v) {
+      g.set(v.id, []);
+    });
+
+    es.forEach(function(e) {
+      var bEdge = {target: e.target.id, flow: e.capacity};
+      var fEdge = {target: e.source.id, flow: 0};
+
+      outgoing = g.get(e.source.id) || [];
+      outgoing.push(bEdge);
+
+      incoming = g.get(e.target.id) || [];
+      incoming.push(fEdge);
+
+      g.set(e.source.id, outgoing);
+      g.set(e.target.id, incoming);
+    });
+
+    return g;
+  }
+  function findAugmentingP(rG) {
+    // run dfs to find s-t path, return the path seq.
+    var explored = d3.map();
+
+    function dfs(path) {
+      var u = path[path.length-1];
+      explored.set(u, true);
+
+      if (u === 1) { return path; }
+
+      // Consider edges which have nonzero residual flow.
+      var es = rG.get(u).filter(function(e){return e.flow>0;});
+
+      if (!es || es.length === 0) { return false; }
+
+      for (var i=0; i<es.length; i++) {
+        var tgt = es[i].target;
+        if (! explored.get(tgt)) {
+          var newpath = path.slice(0);
+          newpath.push(tgt);
+          var p = dfs(newpath);
+          
+          if (p) { return p; }
+        }
+      }
+    }
+    return dfs([0]);
+  }
+  function augment(path, rG) {
+    function bottleneck(path, rG) {
+      // return the min residual capacity of any edge on the path.
+      path = path.reverse();
+      var u = path.pop(),
+          min = parseFloat('Infinity');
+      
+      for (var v; v = path.pop();) {
+        var edge = rG.get(u).filter(function(e) {return e.target === v})[0];
+        if (edge.flow < min) {min = edge.flow;}
+
+        u = v;
+      }
+      return min;
+    }
+
+    var b = bottleneck(path.slice(0), rG);
+    var u = 0;
+    path.forEach(function(v) {
+      if (u === v) {return;}
+
+      var bEdge = rG.get(u).filter(function(e) {return e.target === v})[0];
+      var fEdge = rG.get(v).filter(function(e) {return e.target === u})[0];
+
+      fEdge.flow += b;
+      bEdge.flow -= b;
+
+      u = v;
+    });
+  }
+  // Returns [maxflow, edges]
+  function makeFlow(rG, es) {
+
+    // Display the result flow
+    es.forEach(function(l) {
+      var edge = rG.get(l.target.id).filter(function(e) { return e.target === l.source.id})[0];
+      l.flow = edge.flow;
+    });
+
+    // Display the max flow
+    var sinkIncedent = rG.get(1);
+    var total = 0;
+    sinkIncedent.forEach(function (e) {
+      total += e.flow;
+    });
+
+    return [total, es];
+  }
+
+  path = findAugmentingP(rG);
+  var i = 10;
+  while (path && i>0) {
+    augment(path, rG);
+
+    path = findAugmentingP(rG);
+  }
+
+  console.debug("Residual Graph flow: ", rG);
+  
+  return makeFlow(rG, es);
+}
--- graph.js
+++ graph.js
@@ -1,5 +1,4 @@
 "use strict";
-
 var margin = {top: 20, right: 10, bottom: 20, left: 10};
 var width = 960 - margin.left - margin.right,
     height = 500 - margin.top - margin.bottom,
@@ -21,7 +20,15 @@ var solveButton = d3.select('body').append('button')
     .text("Solve");
 
 // Ford - Fulkerson : Solve for the max flow.
-solveButton.on('click', function(){ fordFulkerson(nodes, links) });
+solveButton.on('click', function(){
+  var res = fordFulkerson(nodes, links),
+      flow = res[0],
+      es = res[1];
+
+  links = es;
+  maxFlow.text(flow);
+  restart();
+});
 
 function textboxHandler() {
   var contents = textBox[0][0].value;
@@ -297,8 +304,7 @@ function restart() {
 
       // select node
       mousedown_node = d;
-      if(mousedown_node === selected_node) {selected_node = null}
-      else {selected_node = mousedown_node}
+      selected_node = mousedown_node
       selected_link = null;
 
       textBox.attr('placeholder', "New Node Name");
@@ -367,15 +373,10 @@ function restart() {
   force.start();
 }
 
-// === Window listeners
-//
+// ========== Listeners ===========
 
-// Mouse handlers
-
+// ========== Mouse Listeners ===========
 function mousedown() {
-  // prevent I-bar on drag
-  
-  // because :active only works in WebKit?
   svg.classed('active', true);
 
   if(d3.event.altKey || mousedown_node || mousedown_link) {return}
@@ -393,17 +394,15 @@ function mousedown() {
 }
 
 function mousemove() {
-  if(!mousedown_node) {return}
+  if (!mousedown_node) {return}
 
-  // update drag line
-  drag_line.attr('d', 'M' + mousedown_node.x + ',' + mousedown_node.y + 'L' + d3.mouse(this)[0] + ',' + d3.mouse(this)[1]);
-
+  drag_line.attr('d', 'M' + mousedown_node.x + ',' + mousedown_node.y +
+                 'L' + d3.mouse(this)[0] + ',' + d3.mouse(this)[1]);
   restart();
 }
 
 function mouseup() {
-  if(mousedown_node) {
-    // hide drag line
+  if (mousedown_node) {
     drag_line
       .classed('hidden', true)
       .style('marker-end', '');
@@ -411,7 +410,6 @@ function mouseup() {
     textBox[0][0].focus();
   }
 
-  // because :active only works in WebKit?
   svg.classed('active', false);
 
   // clear mouse event vars
@@ -428,15 +426,8 @@ function spliceLinksForNode(node) {
 }
 
 // ========== Key Listeners ===========
-
-// only respond once per keydown
-// var lastKeyDown = -1;
-
 function keydown() {
-  // if(lastKeyDown !== -1) return;
-  // lastKeyDown = d3.event.keyCode;
-
-  if(d3.event.altKey) {
+  if (d3.event.altKey) {
     circle.call(drag);
     svg.classed('drag', true);
   }
@@ -447,10 +438,8 @@ function keyup() {
     .on('touchstart.drag', null);
   svg.classed('drag', false);
 }
-
 function fKey() {
-  if(selected_node) {
-    // Make the selected node fixed in position.
+  if (selected_node) {
     selected_node.fixed = true;
   }
   restart();
@@ -459,7 +448,7 @@ function rmObj() {
   // Backspace should not navigate back
   d3.event.preventDefault();
 
-  if(selected_node) {
+  if (selected_node) {
     nodes.splice(nodes.indexOf(selected_node), 1);
     spliceLinksForNode(selected_node);
   } else if(selected_link) {
@@ -469,15 +458,12 @@ function rmObj() {
   selected_node = null;
   restart();
 }
-var win = d3.select(window);
 
-win
+d3.select(window)
     .onKey('backspace/del', rmObj)
     .onKey('f', fKey);
 
-
 // app starts here
-
 svg.on('mousedown', mousedown)
   .on('mousemove', mousemove)
   .on('mouseup', mouseup);
@@ -485,124 +471,3 @@ d3.select(window)
   .on('keydown', keydown)
   .on('keyup', keyup);
 restart();
-
-
-function fordFulkerson(vs, es) {
-  var rG = initResidual(vs,es),
-      path;
-
-  // Initialize a residual graph structure.
-  // returns a map of vetex id's to an array of their outgoing edges.
-  function initResidual(vs,es) {
-    var g = d3.map(),
-        outgoing,
-        incoming;
-
-    vs.forEach(function(v) {
-      g.set(v.id, []);
-    });
-
-    es.forEach(function(e) {
-      var bEdge = {target: e.target.id, flow: e.capacity};
-      var fEdge = {target: e.source.id, flow: 0};
-
-      outgoing = g.get(e.source.id) || [];
-      outgoing.push(bEdge);
-
-      incoming = g.get(e.target.id) || [];
-      incoming.push(fEdge);
-
-      g.set(e.source.id, outgoing);
-      g.set(e.target.id, incoming);
-    });
-
-    return g;
-  }
-  function findAugmentingP(rG) {
-    // run dfs to find s-t path, return the path seq.
-    var explored = d3.map();
-
-    function dfs(path) {
-      var u = path[path.length-1];
-      explored.set(u, true);
-
-      if (u === 1) { return path; }
-
-      // Consider edges which have nonzero residual flow.
-      var es = rG.get(u).filter(function(e){return e.flow>0;});
-
-      if (!es || es.length === 0) { return false; }
-
-      for (var i=0; i<es.length; i++) {
-        var tgt = es[i].target;
-        if (! explored.get(tgt)) {
-          var newpath = path.slice(0);
-          newpath.push(tgt);
-          var p = dfs(newpath);
-          
-          if (p) { return p; }
-        }
-      }
-    }
-    return dfs([0]);
-  }
-  function augment(path, rG) {
-    function bottleneck(path, rG) {
-      // return the min residual capacity of any edge on the path.
-      path = path.reverse();
-      var u = path.pop(),
-          min = parseFloat('Infinity');
-      
-      for (var v; v = path.pop();) {
-        var edge = rG.get(u).filter(function(e) {return e.target === v})[0];
-        if (edge.flow < min) {min = edge.flow;}
-
-        u = v;
-      }
-      return min;
-    }
-
-    var b = bottleneck(path.slice(0), rG);
-    var u = 0;
-    path.forEach(function(v) {
-      if (u === v) {return;}
-
-      var bEdge = rG.get(u).filter(function(e) {return e.target === v})[0];
-      var fEdge = rG.get(v).filter(function(e) {return e.target === u})[0];
-
-      fEdge.flow += b;
-      bEdge.flow -= b;
-
-      u = v;
-    });
-  }
-  function makeFlow(rG) {
-
-    // Display the result flow
-    links.forEach(function(l) {
-      var edge = rG.get(l.target.id).filter(function(e) { return e.target === l.source.id})[0];
-      l.flow = edge.flow;
-    });
-    restart();
-
-    // Display the max flow
-    var sinkIncedent = rG.get(1);
-    var total = 0;
-    sinkIncedent.forEach(function (e) {
-      total += e.flow;
-    });
-    maxFlow.text(total);
-  }
-
-  path = findAugmentingP(rG);
-  var i = 10;
-  while (path && i>0) {
-    augment(path, rG);
-
-    path = findAugmentingP(rG);
-  }
-
-  console.debug("Residual Graph flow: ", rG);
-  
-  makeFlow(rG);
-}
