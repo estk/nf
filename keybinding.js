77bf04e9eda648478bd5379fe1c959177ebc83dd
Evan Simmons <esims89@gmail.com>
2014-04-14 11:46:08 -0700

    Cleaned up keybinding.js.

--- keybinding.js
+++ keybinding.js
@@ -1,5 +1,5 @@
 // How to use keybindings:
-// d3.select('body').onKey('←', function(d,i) {debugger;})
+// d3.select('body').onKey('left', function(d,i) {debugger;})
 (function() {
   "use strict";
   d3.selection.prototype.onKey = function(keyString, cb) {
@@ -28,7 +28,7 @@ d3.keybinding = function(namespace) {
         {return false}
     }
 
-    return (!binding.capture) === (event.eventPhase !== Event.CAPTURING_PHASE);
+    return ( (! binding.capture) === (event.eventPhase !== Event.CAPTURING_PHASE) );
   }
 
   function capture() {
@@ -104,14 +104,10 @@ d3.keybinding = function(namespace) {
   "use strict";
 
   d3.keybinding.modifierCodes = {
-    // Shift key, ⇧
-    '⇧': 16, shift: 16,
-    // CTRL key, on Mac: ⌃
-    '⌃': 17, ctrl: 17,
-    // ALT key, on Mac: ⌥ (Alt)
-    '⌥': 18, alt: 18, option: 18,
-    // META, on Mac: ⌘ (CMD), on Windows (Win), on Linux (Super)
-    '⌘': 91, meta: 91, cmd: 91, 'super': 91, win: 91
+    shift: 16,
+    ctrl: 17,
+    alt: 18, option: 18,
+    meta: 91, cmd: 91, 'super': 91, win: 91
   };
 
   d3.keybinding.modifierProperties = {
@@ -122,67 +118,35 @@ d3.keybinding = function(namespace) {
   };
 
   d3.keybinding.keyCodes = {
-    // Backspace key, on Mac: ⌫ (Backspace)
-    '⌫': 8, backspace: 8,
-    // Tab Key, on Mac: ⇥ (Tab), on Windows ⇥⇥
-    '⇥': 9, '⇆': 9, tab: 9,
-    // Return key, ↩
-    '↩': 13, 'return': 13, enter: 13, '⌅': 13,
-    // Pause/Break key
+    backspace: 8,
+    tab: 9,
+    'return': 13, enter: 13,
     'pause': 19, 'pause-break': 19,
-    // Caps Lock key, ⇪
-    '⇪': 20, caps: 20, 'caps-lock': 20,
-    // Escape key, on Mac: ⎋, on Windows: Esc
-    '⎋': 27, escape: 27, esc: 27,
-    // Space key
+    caps: 20, 'caps-lock': 20,
+    esc: 27, escape: 27, 
     space: 32,
-    // Page-Up key, or pgup, on Mac: ↖
-    '↖': 33, pgup: 33, 'page-up': 33,
-    // Page-Down key, or pgdown, on Mac: ↘
-    '↘': 34, pgdown: 34, 'page-down': 34,
-    // END key, on Mac: ⇟
-    '⇟': 35, end: 35,
-    // HOME key, on Mac: ⇞
-    '⇞': 36, home: 36,
-    // Insert key, or ins
+    pgup: 33, 'page-up': 33,
+    pgdown: 34, 'page-down': 34,
+    end: 35,
+    home: 36,
     ins: 45, insert: 45,
-    // Delete key, on Mac: ⌦ (Delete)
-    '⌦': 46, del: 46, 'delete': 46,
-    // Left Arrow Key, or ←
-    '←': 37, left: 37, 'arrow-left': 37,
-    // Up Arrow Key, or ↑
-    '↑': 38, up: 38, 'arrow-up': 38,
-    // Right Arrow Key, or →
-    '→': 39, right: 39, 'arrow-right': 39,
-    // Up Arrow Key, or ↓
-    '↓': 40, down: 40, 'arrow-down': 40,
-    // odities, printing characters that come out wrong:
-    // Num-Multiply, or *
+    del: 46, 'delete': 46,
+    left: 37, 'arrow-left': 37,
+    up: 38, 'arrow-up': 38,
+    right: 39, 'arrow-right': 39,
+    down: 40, 'arrow-down': 40,
     '*': 106, star: 106, asterisk: 106, multiply: 106,
-    // Num-Plus or +
     '+': 107, 'plus': 107,
-    // Num-Subtract, or -
-    '-': 109, subtract: 109,
-    // Semicolon
+    '-': 109, minus: 109, subtract: 109,
     ';': 186, semicolon:186,
-    // = or equals
     '=': 187, 'equals': 187,
-    // Comma, or ,
     ',': 188, comma: 188,
-    //'-': 189, //???
-    // Period, or ., or full-stop
     '.': 190, period: 190, 'full-stop': 190,
-    // Slash, or /, or forward-slash
     '/': 191, slash: 191, 'forward-slash': 191,
-    // Tick, or `, or back-quote
     '`': 192, tick: 192, 'back-quote': 192,
-    // Open bracket, or [
     '[': 219, 'open-bracket': 219,
-    // Back slash, or \
     '\\': 220, 'back-slash': 220,
-    // Close backet, or ]
     ']': 221, 'close-bracket': 221,
-    // Apostrophe, or Quote, or '
     '\'': 222, quote: 222, apostrophe: 222
   };
 
